c4e29912edfb1769e8b3fc1f642bd826ddc5195e
h4xxel <isaksson.axel@telia.com>
2016-05-05 14:15:24 +0200

    some fixes

--- Makefile
+++ Makefile
@@ -25,6 +25,7 @@ release:
 	@make all
 	@$(MKDIR) release/$(BINFILE)
 	@$(CP) -R bin/* release/$(BINFILE)
+	@$(STRIP) release/$(BINFILE)/$(BINFILE)$(BINEXT)
 	
 package: release
 	@(cd release; zip -r $(BINFILE).zip $(BINFILE))
--- config.mk
+++ config.mk
@@ -5,7 +5,7 @@ MAKEFLAGS	+=	--no-print-directory
 BINFILE		=	birdie26
 
 #General flags
-LDFLAGS		+=	-rdynamic -ldarnit_s
+LDFLAGS		+= -ldarnit_s
 CFLAGS		+=	-Wall -std=c99 -g -O0 -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=600 -I$(TOPDIR)/include
 ASFLAGS		+=
 
@@ -17,10 +17,10 @@ MKDIR		=	mkdir -p
 RMDIR		=	rmdir --ignore-fail-on-non-empty
 CP		=	cp
 
-ifeq (TARGET,i586-mingw32msvc-)
+ifeq ($(TARGET),i586-mingw32msvc-)
 LDFLAGS	+=	-lws2_32 -mwindows bin/res.res
 BINEXT	=.exe
-
 endif
+
 i586-mingw32msvc-: bin
 	@$(WINDRES) platform/windows/res.rc -O coff -o bin/res.res
