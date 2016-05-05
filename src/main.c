be5ef00fce7a9fd5723c1356ca648d32687d7e33
h4xxel <isaksson.axel@telia.com>
2016-05-05 14:41:21 +0200

    darnit template

--- src/main.c
+++ src/main.c
@@ -1,4 +1,20 @@
+#include <darnit/darnit.h>
+#include "ui/ui.h"
+
+#define DISPLAY_WIDTH 800
+#define DISPLAY_HEIGHT 600
 
 int main(int argc, char  **argv) {
+	d_init_custom("birdie26", DISPLAY_WIDTH, DISPLAY_HEIGHT, 0, "birdie26", NULL);
+	ui_init(4);
+	
+	for(;;) {
+		d_render_begin();
+		d_render_blend_enable();
+		d_render_end();
+		d_loop();
+	}
+
+	d_quit();
 	return 0;
 }
