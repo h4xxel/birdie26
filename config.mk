eddb3422ce395b60852489729c79a50c9969ff27
h4xxel <isaksson.axel@telia.com>
2016-05-05 14:32:27 +0200

    muon's ui library.. again

--- config.mk
+++ config.mk
@@ -5,7 +5,7 @@ MAKEFLAGS	+=	--no-print-directory
 BINFILE		=	birdie26
 
 #General flags
-LDFLAGS		+= -ldarnit_s
+LDFLAGS		+= -ldarnit_s -lm -lmodplug -lbz2
 CFLAGS		+=	-Wall -std=c99 -g -O0 -D_POSIX_C_SOURCE=200809L -D_XOPEN_SOURCE=600 -I$(TOPDIR)/include
 ASFLAGS		+=
 
@@ -18,9 +18,13 @@ RMDIR		=	rmdir --ignore-fail-on-non-empty
 CP		=	cp
 
 ifeq ($(TARGET),i586-mingw32msvc-)
-LDFLAGS	+=	-lws2_32 -mwindows bin/res.res
+LDFLAGS	+=	-lws2_32 -lopengl32 -mwindows bin/res.res
 BINEXT	=.exe
 endif
 
 i586-mingw32msvc-: bin
 	@$(TARGET)windres platform/windows/res.rc -O coff -o bin/res.res
+
+ifeq ($(TARGET),)
+LDFLAGS	+= -lSDL -lasound -lGL
+endif
